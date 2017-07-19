dac332bc2cf98d44bab1e0ac806a28ed8711ef80
Koji AOKI <koji@free-quency.com>
2017-07-19 21:18:47 +0900

    Ideal Beauty Defend and Regenerate　ボタン遷移先ヘッダの多言語化漏れ https://redmine.aquadc.suika.bar/issues/3679

--- Share Beauty App/Controllers/NewApproachViewController.swift
+++ Share Beauty App/Controllers/NewApproachViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class NewApproachViewController: UIViewController, NavigationControllerAnnotation {
 
     weak var delegate: NavigationControllerDelegate?
-    var theme: String? = "SHISEIDO's New Approach to Skin"
+    var theme: String? = AppItemTable.getNameByItemId(itemId: 117) //"SHISEIDO's New Approach to Skin"
     var isEnterWithNavigationView = true
     @IBOutlet weak var mBtnDefend: UIButton!
     @IBOutlet weak var mBtnRegenerate: UIButton!
