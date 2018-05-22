f19a1824753c313552c5d8e2da297f36a429615d
takeru.araki <takeru.araki@vantageit.co.jp>
2018-05-22 15:00:22 +0900

    [WIP]SMBK Category 画面途中断面

--- Share Beauty App/Controllers/SMBKCategoryViewController.swift
+++ Share Beauty App/Controllers/SMBKCategoryViewController.swift
@@ -24,18 +24,41 @@ class SMBKCategoryViewController: UIViewController, NavigationControllerAnnotati
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: "08_01.png"), for: .normal)
+        button.setTitle("Liquid Foundation", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.titleLabel!.font = UIFont(name: "Reader-Medium",size: CGFloat(17.7))
+        button.titleEdgeInsets = UIEdgeInsetsMake(7, 71, 0, 0);
+        button.frame = CGRect(x:49,y:60,width:300,height:91)
+        button.layer.borderWidth = 2.0
+        button.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+        self.view.addSubview(button)
     }
+    
     @IBAction func onTapBeautyCategory(_ sender: Any) {
-        let beautyId = productList["\((sender as! UIButton).tag)"]
-        let beautyIds = [beautyId] as! [Int]
-        print("\(beautyIds)")
-        let products = ProductListData(productIds: nil,
-                                       beautyIds: beautyId!.description,
-                                       lineIds: "18").products
-        let productListVc = UIViewController.GetViewControllerFromStoryboard(targetClass: IdealResultViewController.self) as! IdealResultViewController
-        productListVc.products = products
-        delegate?.nextVc(productListVc)
+        let button = sender as! UIButton
+
+        
+        if button.isSelected == true {
+            button.isSelected = false
+            button.layer.borderWidth = 2.0
+            button.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+        } else {
+            button.isSelected = true
+            button.layer.borderWidth = 3.0
+            button.layer.borderColor = UIColor(red: 0.8, green: 0.0, blue: 0.0, alpha: 1.0).cgColor
+        }
+        
+//        let beautyId = productList["\((sender as! UIButton).tag)"]
+//        let beautyIds = [beautyId] as! [Int]
+//        print("\(beautyIds)")
+//        let products = ProductListData(productIds: nil,
+//                                       beautyIds: beautyId!.description,
+//                                       lineIds: "18").products
+//        let productListVc = UIViewController.GetViewControllerFromStoryboard(targetClass: IdealResultViewController.self) as! IdealResultViewController
+//        productListVc.products = products
+//        delegate?.nextVc(productListVc)
     }
     
     override func viewDidLayoutSubviews() {
