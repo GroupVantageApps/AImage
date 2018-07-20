5f05084c629bac823929aca3d5ac3ffabb26ba71
Ryu Ishizuka <ryu.ishiduka@vantageit.co.jp>
2018-07-20 16:10:35 +0900

    [19SSSBA-3] UtmMask DBデータの登録

--- Share Beauty App/Views/UtmMaskEfficacyView.swift
+++ Share Beauty App/Views/UtmMaskEfficacyView.swift
@@ -11,9 +11,12 @@ import Foundation
 class UtmMaskEfficacyView: UIView {
     
     @IBOutlet weak var mScrollV: UIScrollView!
-    
+    @IBOutlet weak var mContentView: UIView!
     @IBOutlet weak var mGraphV: UIView!
     @IBOutlet weak var mPercentageV: UIView!
+    
+    @IBOutlet weak var mTitleLabel: UILabel!
+    @IBOutlet weak var mSubTextLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,65 +25,102 @@ class UtmMaskEfficacyView: UIView {
     func setView() {
         self.mScrollV.frame.size = CGSize(width: self.width, height: self.height)
         self.mScrollV.contentSize = CGSize(width: self.width, height: (self.height) * 2)
+        self.mContentView.size = self.mScrollV.contentSize
         self.mScrollV.isPagingEnabled = true
         self.mScrollV.bounces = false
         
-        let image: UIImage = UIImage(named: "utm_mask_efficacy.png")!
-        let imageV: UIImageView = UIImageView(image: image)
-        imageV.frame = CGRect(x: 0, y: 0, width: self.mScrollV.width, height: self.mScrollV.height)
-        imageV.contentMode = .scaleAspectFit
-        self.mGraphV.addSubview(imageV)
-//        self.mScrollV.addSubview(imageV)
-        
+        self.setGraphContent()
         self.setPercentageContent()
+    }
+    
+    private func setGraphContent() {
+        self.mGraphV.frame = CGRect(x: 0, y: 0, width: self.mScrollV.width, height: self.mScrollV.height)
+        
+        let itemId = 8000
+        self.mTitleLabel.text = AppItemTable.getNameByItemId(itemId: itemId)
+
+        let attributedText = NSMutableAttributedString(string: AppItemTable.getNameByItemId(itemId: itemId + 1)!)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 5.0
+        attributedText.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, attributedText.length))
+        mSubTextLabel.attributedText = attributedText
+        
+        for i in 0...3 {
+            let graphText = UILabel(frame: CGRect(x: 585, y: 230 + i*25, width: 0, height: 0))
+            graphText.font = UIFont(name: "Reader", size: 15)
+            graphText.text = AppItemTable.getNameByItemId(itemId: itemId + 2 + i)
+            if i == 0 {
+                graphText.frame.origin = CGPoint(x: 145, y: 247)
+            } else if i == 2 {
+                graphText.textColor = UIColor(hex: "C11029", alpha: 1.0)
+            } else if i == 3 {
+                graphText.text = AppItemTable.getNameByItemId(itemId: itemId + 3)
+                graphText.frame.origin.y = 415
+            }
+            graphText.sizeToFit()
+            self.mGraphV.addSubview(graphText)
+        }
+        
+        for i in 0...4 {
+            let graphAxis = UILabel(frame: CGRect(x: 125 + (i-1)*70, y: 477, width: 0, height: 0))
+            graphAxis.font = UIFont(name: "Reader", size: 13)
+            graphAxis.text = AppItemTable.getNameByItemId(itemId: itemId + 5 + i)
+            if i == 0 {
+                graphAxis.frame.origin = CGPoint(x: 80, y: 425)
+            } else if i == 4 {
+                graphAxis.frame.origin.x = 550
+            }
+            graphAxis.sizeToFit()
+            self.mGraphV.addSubview(graphAxis)
+        }
+        
+        let text = UILabel(frame: CGRect(x: 650, y: self.mGraphV.height - 30, width: 400, height: 0))
+        text.font = UIFont(name: "Reader-Medium", size: 13)
+        text.text = AppItemTable.getNameByItemId(itemId: itemId + 10)
+        text.textColor = UIColor.lightGray
+        text.textAlignment = .right
+        text.numberOfLines = 0
+        text.sizeToFit()
+        self.mGraphV.addSubview(text)
     }
     
     private func setPercentageContent() {
         self.mPercentageV.frame = CGRect(x: 0, y: self.mScrollV.height, width: self.mScrollV.width, height: self.mScrollV.height)
         
-        let title = UILabel()
-        title.textColor = UIColor.black
+        let itemId = 8011
+
+        let title = UILabel(frame: CGRect(x: 0, y: 30, width: 700, height: 40))
         title.font = UIFont(name: "Reader-Bold", size: 22)
-        title.text = AppItemTable.getNameByItemId(itemId: 8011)
-        title.frame = CGRect(x: 0, y: 30, width: 700, height: 40)
+        title.text = AppItemTable.getNameByItemId(itemId: itemId)
         title.centerX = self.mPercentageV.centerX
         title.textAlignment = .center
         self.mPercentageV.addSubview(title)
         
         for i in 0...2 {
-            let percentLabel = UILabel()
-            percentLabel.textColor = UIColor.black
+            let percentLabel = UILabel(frame: CGRect(x: Int(self.mPercentageV.centerX) - 230, y: 110 + (130 * i), width: 160, height: 82))
             percentLabel.font = UIFont(name: "Reader-Bold", size: 82 )
             percentLabel.textAlignment = .center
-            percentLabel.frame = CGRect(x: Int(self.mPercentageV.centerX) - 230, y: 110 + (130 * i), width: 160, height: 82)
             let perY = Int(percentLabel.frame.origin.y)
-            
             let perTexts = ["86%", "89%", "86%"]
-
             percentLabel.text = perTexts[i]
             
-            let description = UILabel()
-            description.textColor = UIColor.black
+            let description = UILabel(frame: CGRect(x: Int(self.mPercentageV.centerX) - 50, y: perY - 20, width: 350, height: 100))
             description.font = UIFont(name: "Reader", size: 20)
+            description.text = AppItemTable.getNameByItemId(itemId: itemId + i + 1)
             description.numberOfLines = 0
             description.textAlignment = .left
-            description.frame = CGRect(x: Int(self.mPercentageV.centerX) - 50, y: perY - 20, width: 350, height: 100)
-            description.text = AppItemTable.getNameByItemId(itemId: 8011 + i + 1)
             
             self.mPercentageV.addSubview(percentLabel)
             self.mPercentageV.addSubview(description)
         }
         
-        let text = UILabel()
+        let text = UILabel(frame: CGRect(x: self.mPercentageV.width - 330, y: self.mPercentageV.height - 50, width: 300, height: 40))
+        text.font = UIFont(name: "Reader-Medium", size: 13)
+        text.text = AppItemTable.getNameByItemId(itemId: itemId + 4)
         text.textColor = UIColor.lightGray
-        text.font = UIFont(name: "Reader-Medium", size: 12)
-        text.font = text.font.withSize(13)
-        text.textAlignment = .center
+        text.textAlignment = .right
         text.numberOfLines = 0
-        text.frame = CGRect(x: 600, y: 500, width: 400, height: 60)
-        text.text = AppItemTable.getNameByItemId(itemId: 8011 + 4)
         self.mPercentageV.addSubview(text)
-        
-//        self.mScrollV.addSubview(mPercentageV)
+
     }
 }
