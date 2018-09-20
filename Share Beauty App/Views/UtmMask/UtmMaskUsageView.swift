65e38d5eab871248660e14b8eb0d35dfc0c9e2da
Ryu Ishizuka <ryu.ishiduka@vantageit.co.jp>
2018-09-20 18:04:41 +0900

    UtmMask, HowtoUse & Efficacy追加

---
+++ Share Beauty App/Views/UtmMask/UtmMaskUsageView.swift
@@ -0,0 +1,9 @@
+//
+//  UtmMaskUsageView.swift
+//  Share Beauty App
+//
+//  Created by ryu.ishiduka on 2018/09/20.
+//  Copyright © 2018年 AQUA Co., Ltd. All rights reserved.
+//
+
+import Foundation
