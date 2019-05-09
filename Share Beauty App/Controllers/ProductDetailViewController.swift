90f86ca29a3ac9d4252b9b34a48e8b8756bf0240
rui7777 <rui.okura@vantageit.co.jp>
2019-05-09 12:33:22 +0900

    SBA_19AW-40 MAKEUPの新商品にHowto画像を追加(微修正)

--- Share Beauty App/Controllers/ProductDetailViewController.swift
+++ Share Beauty App/Controllers/ProductDetailViewController.swift
@@ -1652,12 +1652,14 @@ class ProductDetailViewController: UIViewController, NavigationControllerAnnotat
                 makeCategoryImages(product.technologyImage)
             }
         case mCategoryButtonHowToUse:
-            mVContent.isHidden = false
             if [625,626,627,628,629,630,631,632].contains(productId) {
+                mVContent.isHidden = false
                 let howToUseV: HowToUse19AW = UINib(nibName: "HowToUse19AW", bundle: nil).instantiate(withOwner: self, options: nil)[0] as! HowToUse19AW
                 howToUseV.setMakeUpHowToUse19AW(productId: productId)
                 self.mVContent.addSubview(howToUseV)
             } else {
+                mVCategoryImage.isHidden = false
+                mVContent.isHidden = true
                 setMakeUpHowToUse(product.usageImage)
             }
             //            makeCategoryImages(product.usageImage)
