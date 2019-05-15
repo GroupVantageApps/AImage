3d3fab6a3fd9c243c54f161f1d2384543e5b06a4
中川明 <a.nakagawa@adminnoMac-mini.local>
2019-05-15 15:53:13 +0900

    BNF619、618のファイル名を620、619に変更

--- Share Beauty App/Views/BNF/BNF620FirstTechView.swift
+++ Share Beauty App/Views/BNF/BNF620FirstTechView.swift
@@ -6,10 +6,10 @@
 //  Copyright © 2018年 AQUA Co., Ltd. All rights reserved.
 //
 
-// TODO: productID 619用なのでファイル名など適切に置き換え
+// TODO: productID 620用なのでファイル名など適切に置き換え　
 import Foundation
 
-class BNF618FirstTechView: UIView {
+class BNF619FirstTechView: UIView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,18 +17,15 @@ class BNF618FirstTechView: UIView {
     }
     
     func setView() {
-        for index in 0...7 {
-            let tag = index + 8276
+        for index in 0...9 {
+            let tag = index + 8323
             let label: UILabel = self.viewWithTag(tag) as! UILabel
-            if [3,4,5,6].contains(index) {
+            if [2,3].contains(index) {
                 let attrStr = NSMutableAttributedString(string: AppItemTable.getNameByItemId(itemId: tag) ?? "not exist")
                 
-                var font_color = UIColor(red: 136/255, green: 183/255, blue: 205/255, alpha: 1)
-                var font_size = CGFloat(18.0)
-                if index == 4 {
-                    font_color = UIColor(red: 109/255, green: 187/255, blue: 192/255, alpha: 1)
-                    font_size = CGFloat(30.0)
-                }
+                let font_color = UIColor(red: 136/255, green: 183/255, blue: 205/255, alpha: 1)
+                let font_size = CGFloat(13.0)
+                
                 attrStr.setFont(UIUtil.getReaderBold(font_size))
                 attrStr.setTextColor(UIColor.white)
                 let shadow = NSShadow()
@@ -46,12 +43,11 @@ class BNF618FirstTechView: UIView {
                 attrStr2.setFont(UIUtil.getReaderBold(font_size))
                 attrStr2.setTextColor(UIColor.white)
                 
-                
                 attrStr2.addAttribute(NSStrokeWidthAttributeName, value: -5.0, range: NSMakeRange(0, attrStr.length))
                 attrStr2.addAttribute(NSStrokeColorAttributeName, value: font_color, range: NSMakeRange(0, attrStr.length))
                 
                 label2.attributedText = attrStr2
-                label2.textAlignment = .center
+                label2.textAlignment = .left
                 label2.adjustsFontSizeToFitWidth = true
                 
                 self.addSubview(label2)
