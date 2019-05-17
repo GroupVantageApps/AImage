f82c061a2025c6e8e5bfd95faa4f1b8af7026546
平井隆彦 <t.hirai@nscp.co.jp>
2019-05-18 08:39:38 +0900

    wip Design FIX

--- Share Beauty App/Views/BNF/BNF619SecondTechView.swift
+++ Share Beauty App/Views/BNF/BNF619SecondTechView.swift
@@ -29,22 +29,22 @@ class BNF619SecondTechView: UIView {
                 shadow.shadowColor = UIColor.white
                 shadow.shadowBlurRadius = 3.0
                 
-                attrStr.addAttribute(NSShadowAttributeName, value: shadow, range: NSMakeRange(0, attrStr.length))
-                attrStr.addAttribute(NSStrokeWidthAttributeName, value: -5.0, range: NSMakeRange(0, attrStr.length))
-                attrStr.addAttribute(NSStrokeColorAttributeName, value: UIColor.white, range: NSMakeRange(0, attrStr.length))
+                // attrStr.addAttribute(NSShadowAttributeName, value: shadow, range: NSMakeRange(0, attrStr.length))
+                // attrStr.addAttribute(NSStrokeWidthAttributeName, value: -5.0, range: NSMakeRange(0, attrStr.length))
+               //  attrStr.addAttribute(NSStrokeColorAttributeName, value: UIColor.white, range: NSMakeRange(0, attrStr.length))
                 
                 label.attributedText = attrStr
                 label.adjustsFontSizeToFitWidth = true
                 
                 let label2: UILabel = UILabel.init(frame: label.frame)
                 
-                let attrStr2 = NSMutableAttributedString(string: AppItemTable.getNameByItemId(itemId: tag) ?? "not exist")
-                attrStr2.setFont(UIUtil.getReaderMedium(12))
+                //let attrStr2 = NSMutableAttributedString(string: AppItemTable.getNameByItemId(itemId: tag) ?? "not exist")
+                //attrStr2.setFont(UIUtil.getReaderMedium(12))
                 
-                label2.attributedText = attrStr2
-                label2.numberOfLines = 2
-                label2.textAlignment = .center
-                label2.adjustsFontSizeToFitWidth = true
+                //label2.attributedText = attrStr2
+                // label2.numberOfLines = 2
+                //label2.textAlignment = .center
+                //label2.adjustsFontSizeToFitWidth = true
                 
                 self.addSubview(label2)
                 
