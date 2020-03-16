93778ed3d393d8cda80da2c1289bfaa32f6c9805
madoka.igarashi <madoka.igarashi@vantageapps.co.jp>
2020-03-16 10:40:13 +0900

    release 20200316

--- Share Beauty App/Common/DownloadConfigure.swift
+++ Share Beauty App/Common/DownloadConfigure.swift
@@ -74,11 +74,12 @@ class DownloadConfigure {
             if let value = UserDefaultKey.target.get() {
                 return Target(rawValue: value as! Int)!
             } else {
-                //UserDefaultKey.target.set(value: Target.release.rawValue)//DB向き 本番
-                //return Target.release
                 //DB向き 本番
-                 UserDefaultKey.target.set(value: Target.develop.rawValue)//DB向き　develop or release 無理やり
-                return Target.develop //DB向き　develop or release 無理やり
+                UserDefaultKey.target.set(value: Target.release.rawValue)//DB向き 本番
+                return Target.release
+                //DB向き 開発
+////                 UserDefaultKey.target.set(value: Target.develop.rawValue)//DB向き　develop or release 無理やり
+//                return Target.develop //DB向き　develop or release 無理やり
             }
         }
         set (value) {
