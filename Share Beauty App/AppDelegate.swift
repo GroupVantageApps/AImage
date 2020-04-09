2d5f72533fdfeb8b00ed4002579ef36f737c9123
Yuya-Tominaga-VIT <yuya.tominaga@vantageit.co.jp>
2020-04-09 13:45:28 +0900

    fix: コンパイルエラーの間違った修正を修正

--- Share Beauty App/AppDelegate.swift
+++ Share Beauty App/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?       
 
-    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //TouchVisualizer t-hirai
         // Initialize with config - octocat
@@ -47,7 +47,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    private func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         print("url=========")
         print(url)
         // example: ssdsba://product?product_id=XXX
